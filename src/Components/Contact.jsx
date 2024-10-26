c9e907dd0bb8903aee158b818c1c8dcf15dbd93e
Virender Prasad <virender288@gmail.com>
2024-10-26 18:52:23 +0530

    edited the box css and reapplied some UX and UI changes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div className="form4" id='contact'>
       <form action="">
-        <span>Contact Form</span>
+        <span>Contact Us</span>
         <input type="text" placeholder="First Name" required />
         <input type="text" placeholder="Last Name" required />
         <input type="email" placeholder="Email" required />
